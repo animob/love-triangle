902c644d9ed04575447915cb7d2e752f0ba51d1e
Animob <anngwork@gmail.com>
2019-03-16 14:02:20 +0300

    feat: change index.js. Love triangle problem solved

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var widthPreferences = preferences.length;
+  var numberTriangles = 0;
+  
+  for (i = 0; i < widthPreferences; i++) {
+    numberOne = i + 1;
+    valueOne = preferences[i];
+
+    numberTwo = valueOne;
+    valueTwo = preferences[numberTwo - 1];
+
+    numberThree = valueTwo;
+    valueThree = preferences[numberThree - 1];
+
+    if (valueThree == numberOne && numberOne != numberThree) {
+      numberTriangles += 1;
+    }
+
+  }
+  return (numberTriangles / 3);
 };
